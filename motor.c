e33b15136e2bf0fdf6cd39f21b0c089cdbad360f
MD-11F-RR <royals6234@gmail.com>
2017-02-18 00:23:05 +0800

    差速功能添加
    修改left和right函数，使执行时差速，转向和配合未测试

--- motor.c
+++ motor.c
@@ -95,16 +95,16 @@ void motor_ccw() {
 //电机逆时针
 
 void motor_left() {
-  pwm_motorL = MID_POINT;
+  pwm_motorL = MID_POINT + step / 2;
   pwm_motorR = MID_POINT + step;
 }
-//左转弯，左侧电机停止，右侧电机以原先速度转动
+//左转弯，左侧电机以0.5倍速度转，右侧电机以原先速度转动，差速测试
 
 void motor_right() {
   pwm_motorL = MID_POINT - step;
-  pwm_motorR = MID_POINT;
+  pwm_motorR = MID_POINT - step / 2;
 }
-//右转弯，右侧电机停止，左侧电机以原先速度转动
+//右转弯，右侧电机以0.5倍速度转，左侧电机以原先速度转动，差速测试
 
 void motor_cw() {
   pwm_motorL = MID_POINT - step;
