54dd09b7a8fa587567d45d134909da9b61caddf9
MD-11F-RR <royals6234@gmail.com>
2017-02-22 22:57:31 +0800

    架构构建
    构建灭火时转向架构及引入滤波，有待测试内容

--- fire.c
+++ fire.c
@@ -1,5 +1,6 @@
 #include "AT89X52.H"
 #include "motor.h"
+#include "Boebot.h"
 
 sbit fire_sensor_L1 = P1^6;
 sbit fire_sensor_L2 = P1^7;
@@ -17,24 +18,41 @@ sbit fire_fan = P2^4;
 
 char fire_detected() {
   if (fire_sensor_R1 == 0) {
-    return 5;
+		delay_nus(20);
+		if (fire_sensor_R1 == 0){
+			return 5;
+		}
   }
   else if (fire_sensor_R2 == 0) {
-    return 4;
+		delay_nus(20);
+		if (fire_sensor_R2 == 0){
+			return 4;
+		}
   }
   else if (fire_sensor_M == 0) {
-    return 3;
+		delay_nus(20);
+		if (fire_sensor_M == 0){
+			return 3;
+		}
   }
   else if (fire_sensor_L1 == 0) {
-    return 1;
+		delay_nus(20);
+		if (fire_sensor_L1 == 0){
+			return 1;
+		}
   }
   else if (fire_sensor_L2 == 0) {
-    return 2;
+		delay_nus(20);
+		if (fire_sensor_L2 == 0){
+			return 2;
+		}
   }
   else{
     return 0;
   }
-  //检测函数，任意一个火焰传感器检测到火焰就返回true
+  //检测函数，任意一个火焰传感器检测到火，消去抖动后即返回检测到火的传感器
+	//从左到右L1	L2	M	R2	R1	分别返回1	2	3	4	5
+	//返回0代表没有检测到火焰
 }
 
 void fire_putout() {
@@ -47,6 +65,7 @@ void fire_putout() {
   		case 3:
   		motor_stop();
   		break;
+			//中间传感器检测到火焰后停止电机
 
   		case 2:
   		motor_speed(2);
@@ -57,6 +76,7 @@ void fire_putout() {
   		motor_speed(2);
   		motor_cw();
   		break;
+			//靠内两个传感器检测到火焰后减速转向
 
       case 1:
   		motor_speed(3);
@@ -67,14 +87,25 @@ void fire_putout() {
       motor_speed(3);
       motor_cw();
       break;
+
+			default:
+			//default不做任何动作，延续之前动作
+			break;
   	}
-    while(fire_detected != 3);
-    
+    while(fire_detected != 3){
+			if (0) {
+				break;
+				//预留，当误检测时允许取消操作
+			}
+		}
+
     if (fire_detected() == 3) {
       fire_fan = 0;
       while(!fire_detected());
       fire_fan = 1;
+
       break;
+			//不知道能不能break出大的while1，有待测试！
     }
   }
 }
--- main.c
+++ main.c
@@ -2,13 +2,14 @@
 #include "motor.h"
 #include "route_track.h"
 #include "fire.h"
+#include "Boebot.h"
 
 int main() {
   motor_init();
   motor_speed(3);
 	EA = 1;
   while (1) {
-    /*
+		/*
     //route_track_track();
     if (fire_detected() != 0) {
       while(fire_detected() != 3);
@@ -22,8 +23,19 @@ int main() {
     }
 
     //fire_fan_test(1);
-    */
+
     motor_stop();
+		*/
+		motor_stop();
+		while (1) {
+			motor_forward();
+
+			delay_nms(1000);
+			if (1) {
+				break;
+			}
+		}
+		//测试代码，检测是否能够成功break出大的while1
   }
   return 0;
 }
